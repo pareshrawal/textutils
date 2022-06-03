d3c352ac4f64aef26304acd143840c2fe5368423
Amitesh <amitesh.rwl543@gmail.com>
2022-06-03 15:44:49 +0530

    Updates

---
+++ precache-manifest.96b8ba4dbb976aa938ac7c5a2ff0e85e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c627f91f1535b1980cb4",
+    "url": "/TextUtils-React/static/css/main.6805e52c.chunk.css"
+  },
+  {
+    "revision": "c627f91f1535b1980cb4",
+    "url": "/TextUtils-React/static/js/main.c627f91f.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils-React/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fea4a0829c121c975406",
+    "url": "/TextUtils-React/static/js/2.fea4a082.chunk.js"
+  },
+  {
+    "revision": "0d79c3c2a5762a692cdb",
+    "url": "/TextUtils-React/static/js/runtime~main.0d79c3c2.js"
+  },
+  {
+    "revision": "a92bcaebf47fe051cf59643f156d1f12",
+    "url": "/TextUtils-React/index.html"
+  }
+];
